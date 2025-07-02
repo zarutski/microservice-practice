1d5b97ef446c74b888692809fb920d2412fce89d
Maksim Zarutski <zarutski93maksim@gmail.com>
2025-07-02 17:37:49 +0300

    producer: product creation (async message publishing)

--- src/main/java/com/learn/self/kafka/product/microservice/service/ProductServiceImpl.java
+++ src/main/java/com/learn/self/kafka/product/microservice/service/ProductServiceImpl.java
@@ -2,15 +2,20 @@ package com.learn.self.kafka.product.microservice.service;
 
 import com.learn.self.kafka.product.microservice.dto.CreateProductDTO;
 import com.learn.self.kafka.product.microservice.service.event.ProductCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    private KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+    private final KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -25,11 +30,21 @@ public class ProductServiceImpl implements ProductService {
         ProductCreatedEvent createdEvent = new ProductCreatedEvent(productId, createProductDTO.getTitle(),
                 createProductDTO.getPrice(), createProductDTO.getQuantity());
 
-        // publish message via KafkaTemplate (wrapper around Kafka Producer provided by Spring) - async mode (by default)
-        kafkaTemplate.send("payment-created-events-topic", productId, createdEvent);
+        // publish message via KafkaTemplate (wrapper around Kafka Producer provided by Spring) - async mode (by default) and retrieve result (CompletableFuture usage - async)
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future =
+                kafkaTemplate.send("payment-created-events-topic", productId, createdEvent);
 
-        // TODO: async response processing
-        return "";
+        // async response processing
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                LOGGER.error("Failed to send message: {}", ex.getMessage());
+            } else {
+                LOGGER.info("Messege send successfully: {}", result.getRecordMetadata()); // metadata here: kafka provided class for message metadata (offset, topic, partition ...)
+            }
+        });
+
+        LOGGER.info("Return: {}", productId); // testing async mode (this log will appear before any callback log is printed)
+        return productId;
     }
 
 }
