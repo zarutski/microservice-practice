ccc7a81988828053ae132b09c30503d61817b0af
Maksim Zarutski <zarutski93maksim@gmail.com>
2025-08-20 18:43:32 +0300

    producer: java-config (properties-based only config replacement)

--- src/main/java/com/learn/self/kafka/product/microservice/config/KafkaConfig.java
+++ src/main/java/com/learn/self/kafka/product/microservice/config/KafkaConfig.java
@@ -1,21 +1,70 @@
 package com.learn.self.kafka.product.microservice.config;
 
+import com.learn.self.kafka.product.microservice.service.event.ProductCreatedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
+
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private String deliveryTimeout;
+
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private String linger;
+
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private String requestTimeout;
+
+    Map<String, Object> producerConfigs() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.ACKS_CONFIG, acks);
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
+        config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+        return config;
+    }
+
+    // more flexible java-config - properties only config replacement (dynamic config, easier testing)
+    @Bean
+    ProducerFactory<String, ProductCreatedEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
     // describe bean to create new topic at the application startup
     @Bean
     NewTopic createTopic() {
-        return TopicBuilder.name("product-created-events-topic")
-                .partitions(3)
-                .replicas(3) // each partition will be stored on 3 brokers (1 leader + 2 followers); max possible if the cluster has 3 brokers
+        return TopicBuilder.name("product-created-events-topic").partitions(3).replicas(3) // each partition will be stored on 3 brokers (1 leader + 2 followers); max possible if the cluster has 3 brokers
                 .configs(Map.of("min.insync.replicas", "2")) // additional property: minimum number of in-sync/synchronized replicas (including leader) that must acknowledge the message
                 .build();
     }
